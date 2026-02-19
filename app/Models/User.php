75dfd88b08194c21e3877bdd983c1094980cb907
Melisa Eugenia Rivas Linares <20245324@esen.edu.sv>
2026-02-18 19:02:45 -0600

    agregando comentarios explicativos

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,9 +8,15 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ *  indica que este modelo es el usuario autenticable del sistema (login, tokens, etc). 
+ *  Los modelos normales extienden solo Model.
+ */
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
+    /** en lugar de borrar el registro de la BD, agrega un campo deleted_at. 
+     * El registro sigue existiendo pero Laravel lo ignora en las queries.  */
     use HasFactory, Notifiable, SoftDeletes;
 
     /**
@@ -34,6 +40,8 @@ class User extends Authenticatable
      * The attributes that should be hidden for serialization.
      *
      * @var list<string>
+     * 
+     * campos que se omiten cuando retornas JSON. Por eso nunca ves el password en las respuestas.
      */
     protected $hidden = [
         'password',
@@ -44,14 +52,15 @@ class User extends Authenticatable
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
+     * Le dice a Laravel cómo transformar un valor al leerlo o escribirlo:
      */
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-            'birthdate' => 'date',
-            'hiring_date' => 'date',
+            'email_verified_at' => 'datetime', // escribe la fecha y hora
+            'password' => 'hashed', // la guarda hasheada, no en texto plano
+            'birthdate' => 'date', // solo la fecha, sin hora
+            'hiring_date' => 'date', // solo la fecha, sin hora
         ];
     }
 }
